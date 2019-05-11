dfea3a992e9b2a56c47176febe9f53ef0613ba0e
DenisShilyaev <79194954002@yandex.ru>
2019-05-11 21:05:27 +0500

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var addButton = document.getElementById('add');
 var inputTask = document.getElementById('new-task');
-var unfinishedTask = document.getElementById('unfinished-tasks');
-var finishedTask = document.getElementById('finished-tasks');
+var unfinishedTasks = document.getElementById('unfinished-tasks');
+var finishedTasks = document.getElementById('finished-tasks');
 
-function createNewElement (task) {
+function createNewElement (task, finished) {
     var listItem = document.createElement ('li');
     var checkBox = document.createElement ('button');
+
+    if (finished) {
     checkBox.className = "material-icons checkBox";
-    checkBox.innerHTML = "<i class='material-icons'>check_box_outline_blank</i>";
+    checkBox.innerHTML = "<i class='material-icons'>check_box</i>";     
+    } else {
+    checkBox.className = "material-icons checkBox";
+    checkBox.innerHTML = "<i class='material-icons'>check_box_outline_blank</i>"; 
+    }
+
     var label = document.createElement ('label');
     label.innerText = task;
     var input = document.createElement ('input');
@@ -30,11 +37,13 @@ function createNewElement (task) {
 
 function addTask () {
     if (inputTask.value) {
-        var listItem = createNewElement (inputTask.value);
-        unfinishedTask.appendChild (listItem);
+        var listItem = createNewElement (inputTask.value, false);
+        unfinishedTasks.appendChild (listItem);
         bindTaskEvents (listItem, finishTask);
         inputTask.value = "";
     }
+
+    save (); 
 }
 
 addButton.onclick = addTask;
@@ -43,24 +52,26 @@ function deleteTask () {
     var listItem = this.parentNode;
     var ul = listItem.parentNode;
     ul.removeChild (listItem);
+    save (); 
 }
 
 function editTask () {
     var editButton = this;
     var listItem = this.parentNode;
     var label = listItem.querySelector ('label');
-    var input = listItem.querySelector ('input [type = text]');
+    var input = listItem.querySelector ('input[type = text]');
 
     var containsClass = listItem.classList.contains ('editMode');
 
     if (containsClass) {
-        label.innerText = imput.value;
+        label.innerText = input.value;
         editButton.className = "material-icons edit";
         editButton.innerHTML = "<i class='material-icons'>edit</i>";
+        save (); 
     } else {
-        input.value = label.innerTextl;
-        editButton.className = "material-icons edit";
-        editButton.innerHTML = "<i class='material-icons'>edit</i>";
+        input.value = label.innerText;
+        editButton.className = "material-icons save";
+        editButton.innerHTML = "<i class='material-icons'>save</i>";
     }
 
     listItem.classList.toggle ('editMode');
@@ -74,6 +85,7 @@ function finishTask () {
 
     finishedTasks.appendChild (listItem);
     bindTaskEvents (listItem, unfinishTask);
+    save (); 
 }
 
 function unfinishTask () {
@@ -84,6 +96,7 @@ function unfinishTask () {
 
     unfinishedTasks.appendChild (listItem);
     bindTaskEvents (listItem, finishTask);
+    save (); 
 }
 
 function bindTaskEvents (listItem, checkBoxEvent) {
@@ -94,4 +107,45 @@ function bindTaskEvents (listItem, checkBoxEvent) {
     checkBox.onclick = checkBoxEvent;
     editButton.onclick = editTask;
     deleteButton.onclick = deleteTask;
+}
+
+function save () {  //Для сохранения задач (что бы не удалялись после обновления страницы)
+    var finishedTaskArr = [];
+    var unfinishedTaskArr = [];
+
+    for (var i = 0; i<unfinishedTasks.children.length; i++ ) {
+        unfinishedTaskArr.push(unfinishedTasks.children[i].getElementsByTagName('label')[0].innerText);
+    }
+
+    for (var i = 0; i<finishedTasks.children.length; i++ ) {
+        finishedTaskArr.push(finishedTasks.children[i].getElementsByTagName('label')[0].innerText);
+    }
+    localStorage.removeItem ('todo');
+    localStorage.setItem ('todo', JSON.stringify({
+        unfinishedTasks: unfinishedTaskArr,
+        finishedTasks: finishedTaskArr
+    }));
+}
+
+function load () {
+
+    return JSON.parse (localStorage.getItem ('todo'))
+}
+
+var data = load ();
+
+for (var i = 0; i < data.unfinishedTasks.length; i++){
+    var listItem = createNewElement (data.unfinishedTasks[i], false);
+    unfinishedTasks.appendChild(listItem);
+    bindTaskEvents (listItem, finishTask);
+}
+
+for (var i = 0; i < data.finishedTasks.length; i++){
+    var listItem = createNewElement (data.finishedTasks[i], true);
+    finishedTasks.appendChild(listItem);
+    bindTaskEvents (listItem, finishTask);
+}
+
+function load(){
+    return JSON.parse(localStorage.getItem('todo'));
 }
